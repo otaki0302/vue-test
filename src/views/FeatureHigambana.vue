9b71f7d4c7ef853ab3f9e6544d87fd632e14585e
otaki0302 <s.ootaki@bud-international.co.jp>
2025-03-14 11:31:09 +0900

    Swiperが動作するよう修正

--- src/views/FeatureHigambana.vue
+++ src/views/FeatureHigambana.vue
@@ -12,7 +12,7 @@
                <h2 class="sec-ttl">彼岸花が見られる名所</h2>
                <div class="sec--lead__col">
                     <swiper class="sec--lead__slide"
-                        :modules="modules"
+                        :modules="[Navigation, Pagination, A11y, Autoplay]"
                         :slides-per-view="1"
                         :loop="true"
                         :space-between="25"
@@ -40,7 +40,7 @@
                 </div>
                 <div class="sec--lead__col">
                     <swiper class="sec--lead__slide"
-                        :modules="modules"
+                        :modules="[Navigation, Pagination, A11y, Autoplay]"
                         :slides-per-view="1"
                         :loop="true"
                         :space-between="25"
@@ -249,39 +249,24 @@
         gap: 30px;
     }
 </style>
-<style>
-
-</style>
 <script>
-  // import Swiper core and required modules
-  import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/vue';
 
-  // Import Swiper Vue.js components
-  import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
 
-  // Import Swiper styles
-  import 'swiper/css';
-  import 'swiper/css/navigation';
-  import 'swiper/css/pagination';
-
-  // Import Swiper styles
-  export default {
-    components: {
-      Swiper,
-      SwiperSlide,
-    },
-    setup() {
-      const onSwiper = (swiper) => {
-        console.log(swiper);
-      };
-      const onSlideChange = () => {
-        console.log('slide change');
-      };
-      return {
-        onSwiper,
-        onSlideChange,
-        modules: [Navigation, Pagination, A11y, Autoplay],
-      };
-    },
-  };
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Navigation, Pagination, A11y, Autoplay],
+    };
+  },
+};
 </script>
